a8801dde659aa0df05677e6d92bb293286adc548
aushacker <25114336+aushacker@users.noreply.github.com>
2024-01-08 10:25:31 +1100

    fix broken chapter1 code

---
+++ chapter1/bad.c
@@ -0,0 +1,26 @@
+/* This may look like nonsense, but really is -*- mode: C -*- */
+#include <stdio.h>
+
+/* The main thing that this program does. */
+int main() {
+  // Declarations
+  int i;
+  double A[5] = {
+    9.0,
+    2.9,
+    0.0,
+    0.00007,
+    3.E+25,
+  };
+
+  // Doing some work
+  for(i = 0; i < 5; ++i) {
+    printf("element %d is %g, \tits square is %g\n",
+      i,
+      A[i],
+      A[i]*A[i]
+    );
+  }
+
+  return 0;
+}
